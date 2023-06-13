c4e4a1cdcd2716689aac78e55c25ddda6ec9e769
Fakrul Islam <dev.rupok02@gmail.com>
2023-06-13 17:18:45 +0600

    All summury here

---
+++ ES6-recap.js
@@ -0,0 +1,30 @@
+/*
+Must know:
+1. var, let, constant
+2. default parameter
+3. template string
+    3.1: create multiline string(``)
+    3.2: add dynamic variable (expression): `${}`
+4. Arrow function
+    4.1: Single parameter arrow function
+    4.2: two parameters arrow function
+    4.3: arrow function with function body. And dont forget you have to 
+    return explicitly
+    4.4: No parameter arrow function
+5. Spread operator: [...numbers, 12, 45]
+6. Destructuring
+    6.1: object destructuring. How to get property name as variable
+    const {x} = {x: 3, y: 5}
+    6.2: Array destructuring
+    const [a, b] = [3, 8, 12, 56]
+7. map, filter, find
+8. forEach
+9. reduce
+10. Optional chaining
+11. class, object, constructor, inheritance
+12. prototypical chain
+13. Dot notation vs bracket notation
+
+
+
+*/
